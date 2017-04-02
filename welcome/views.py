b30d2df381c687b7d0e1b32a6c5a515742c1cd58
Grant Saunders <grant.saunders@gmail.com>
2017-04-02 14:04:03 -0400

    added events to view

--- welcome/views.py
+++ welcome/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from welcome.models import Event
+from datetime import date
 
 
 # Create your views here.
@@ -12,4 +13,9 @@ def alpha(request):
 
 
 def index(request):
-    return render(request, 'welcome/index.html', events=Event.objects )
+    today = date.today()
+    past_events = Event.objects.filter(date__lt=today, visible__exact=True)
+    upcoming_events = Event.objects.filter(date__gte=today, visible__exact=True)
+    #upcoming_events = Event.objects.get(date>=today, visible=True)
+
+    return render(request, 'welcome/index.html', {'past_events': past_events}, {'upcoming_events': upcoming_events})
