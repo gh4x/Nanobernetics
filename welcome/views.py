76005b6739b1b7d8ea21e6de5896764054ea2b39
Grant Saunders <grant.saunders@gmail.com>
2017-04-02 13:09:54 -0400

    added event model

--- welcome/views.py
+++ welcome/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from welcome.models import Event
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'welcome/home.html', {'message': 'Nano Bernetics!'})
+    return render(request, 'welcome/home.html', {'message': 'Nanobernetics!'})
 
 
 def alpha(request):
-    return render(request, 'welcome/alpha.html', {'message': 'Nano Bernetics!'})
+    return render(request, 'welcome/alpha.html', {'message': 'Nanobernetics!'})
 
 
 def index(request):
-    return render(request, 'welcome/index.html', {'message': 'Nano Bernetics!'})
+    return render(request, 'welcome/index.html', events=Event.objects )
